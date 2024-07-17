2b7dd8f35aef4ec107b6a98f083fefa0edf2385f
Israel Albino Galvan <israel.galvan@live.com>
2024-07-17 18:24:29 -0300

    Reverted Host

--- Luna.Core/Host.cs
+++ Luna.Core/Host.cs
@@ -4,50 +4,35 @@ public class Host
 {
     public Node Root { get; set; }
 
-    private readonly NodeCallback ConfigHandler = () => {};
-    private readonly NodeCallback AwakeHandler = () => {};
-    private readonly NodeCallback StartHandler = () => {};
-    private readonly NodeCallback EarlyUpdateHandler = () => {};
-    private readonly NodeCallback UpdateHandler = () => {};
-    private readonly NodeCallback LateUpdateHandler = () => {};
-    private readonly NodeCallback RenderHandler = () => {};
     private readonly InputHandler InputHandler = _ => {};
 
     public Host(Node root)
     {
         Root = root;
-
-        ConfigHandler += Root.Config;
-        AwakeHandler += Root.Awake;
-        StartHandler += Root.Start;
-        EarlyUpdateHandler += Root.EarlyUpdate;
-        UpdateHandler += Root.Update;
-        LateUpdateHandler += Root.LateUpdate;
-        RenderHandler += Root.Render;
         InputHandler += Root.Input;
     }
 
     public void Run()
     {
-        ConfigHandler();
+        Root.Config();
 
         Window.EngineWindow.Init();
 
         Injector.Get<IWindow>().SetKeyCallback((key, action, mods) 
             => InputHandler(new KeyboardEvent(key, action, mods)));
 
-       AwakeHandler();
-       StartHandler();
+       Root.Awake();
+       Root.Start();
 
         while (Window.Running)
         {
-            EarlyUpdateHandler();
-            UpdateHandler();
-            LateUpdateHandler();
+            Root.EarlyUpdate();
+            Root.Update();
+            Root.LateUpdate();
             Physics.FixedUpdate();
 
             Window.EngineWindow.BeginRender();
-            RenderHandler();
+            Root.Render();
             Window.EngineWindow.EndRender();
         }
 
